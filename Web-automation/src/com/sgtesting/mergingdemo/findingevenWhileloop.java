f4e440807ba7066a5c96beb9f84be03279c2c5d3
harishmulimani <harishmulimani1998@gmail.com>
2023-01-02 17:52:30 +0530

    testing different loops

---
+++ Web-automation/src/com/sgtesting/mergingdemo/findingevenWhileloop.java
@@ -0,0 +1,16 @@
+package com.sgtesting.mergingdemo;
+
+public class findingevenWhileloop {
+	public static void main(String args[]) {
+		int e=0;
+		while(e<=100) {
+			if(e%2==0) {
+				System.out.println(e);
+		
+			}
+			e++;
+		}
+		
+	}
+
+}
