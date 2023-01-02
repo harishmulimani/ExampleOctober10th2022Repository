8a13c06161ce1f90f8f61c2d26e2db064bd818fa
harishmulimani <harishmulimani1998@gmail.com>
2023-01-02 17:24:41 +0530

    testing finding even number scenario

---
+++ Web-automation/src/com/sgtesting/mergingdemo/Findingevennumbers.java
@@ -0,0 +1,17 @@
+package com.sgtesting.mergingdemo;
+
+public class Findingevennumbers {
+
+	public static void main(String[] args) {
+		
+	  
+	  System.out.println("even  numbers between 0 to 100");
+	  for(int e=0;e<=100;e++) {
+		  if (e%2==0) {
+			  System.out.println(e);
+		  }
+	  }
+		
+	}
+
+}
